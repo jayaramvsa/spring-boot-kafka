e4e2492aeef5c56bc5949de886ff552d38838f6c
jayaramvsa <jayaramvsa@gmail.com>
2021-06-17 20:42:39 +0530

    Modified the controller Classs to accept the message and persist into topic

--- src/main/java/io/jay/kafka/controller/LibraryEventsController.java
+++ src/main/java/io/jay/kafka/controller/LibraryEventsController.java
@@ -1,6 +1,9 @@
 package io.jay.kafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.jay.kafka.domain.LibraryEvent;
+import io.jay.kafka.producer.LibraryEventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LibraryEventsController {
 
-    @PostMapping("/v1/libraryEvent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
 
+    @PostMapping("/v1/libraryEvent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }

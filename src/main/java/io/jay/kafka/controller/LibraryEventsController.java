9e0453240380325690a5ef9449e27083ef18520b
jayaramvsa <jayaramvsa@gmail.com>
2021-06-17 20:56:33 +0530

    Modified logs and application.yml

--- src/main/java/io/jay/kafka/controller/LibraryEventsController.java
+++ src/main/java/io/jay/kafka/controller/LibraryEventsController.java
@@ -3,6 +3,7 @@ package io.jay.kafka.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.jay.kafka.domain.LibraryEvent;
 import io.jay.kafka.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class LibraryEventsController {
 
     @Autowired
@@ -19,7 +21,9 @@ public class LibraryEventsController {
     @PostMapping("/v1/libraryEvent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
 
+        log.info("before SendLibraryEvent");
         libraryEventProducer.sendLibraryEvent(libraryEvent);
+        log.info("after sendLibraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }

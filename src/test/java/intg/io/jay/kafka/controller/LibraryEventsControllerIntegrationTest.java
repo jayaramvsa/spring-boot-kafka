9cbc061fd6a834f8bc84153a9ef0e1cf11a00b13
jayaramvsa <jayaramvsa@gmail.com>
2021-06-18 23:17:30 +0530

    Added Integration TestCase

---
+++ src/test/java/intg/io/jay/kafka/controller/LibraryEventsControllerIntegrationTest.java
@@ -0,0 +1,36 @@
+package io.jay.kafka.controller;
+
+import io.jay.kafka.domain.Book;
+import io.jay.kafka.domain.LibraryEvent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LibraryEventsControllerIntegrationTest {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    void postLibraryEvent() {
+
+        //given
+        Book book = Book.builder().bookId(1).bookAuthor("Hamang Lee").bookName("How To Train a Dragon").build();
+        LibraryEvent libraryEvent = LibraryEvent.builder().libraryEventId(1).book(book).build();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Content-type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> request = new HttpEntity<LibraryEvent>(libraryEvent, httpHeaders);
+
+        //when
+        ResponseEntity<LibraryEvent> responseEntity = testRestTemplate.exchange("/v1/libraryEvent", HttpMethod.POST, request, LibraryEvent.class);
+
+        //then
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    }
+}
